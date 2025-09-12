a8cc57fef37de333fc99eae85800d6ba8b73751a
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-09-12 11:23:07 -0300

    Função com conexão do banco adicionado

--- php/db.php
+++ php/db.php
@@ -1,7 +1,19 @@
 <?php 
-    $pdo = new PDO("mysql:host=10.91.47.77;dbname=modelophp_db", "root", "123",
-    [
-        
-    ]);
+    // Criar um função para conectar com o banco, que retorne um objeto PDO
+    // Criar um objeto nulo estático
+    // Verificar se esse objeto é nulo
+    // Adicione referencia do objeto para á classe PDO ( com host de conexão  e propriedades )
+    // Retorno o objeto do tipo PDO
 
+    function getConnection() : PDO{
+        static $pdo;
+        if ($pdo == null){
+            $pdo = new PDO("mysql:host=10.91.47.77;dbname=modelophp_db",
+             "root", 
+             "123", 
+             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+        }
+        return $pdo;
+    }
 ?>
